af22230e50b3e3e61695e62fac9d3e2eca4493a6
LuisGSandoval <luis.sandoval.457@outlook.com>
2019-12-05 03:10:35 -0500

    Se agrega un fmPrintln para ver los valorse

--- structs.go
+++ structs.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Car is a structs => data encapsulation
 // A struct is an abstract datatype that groups together logicly related data
 type Car struct {
@@ -9,5 +11,12 @@ type Car struct {
 
 func main() {
 	// Hay 2 formas de inicializar (agregar data en)  un struct la primer opcion es:
-	c := Car{"Mazda", 2019} // Sin agregar el key de los valores
+	// c := Car{"Mazda", 2019} // Sin agregar el key de los valores
+	c := Car{
+		Brand: "Mazda",
+		Model: 2019,
+	} // Agregando el key de los valores (recommended)
+
+	fmt.Println(c)
+
 }
